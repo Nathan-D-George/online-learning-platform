8ac400ba7c9c415a0638ebb0db6842aba930b2b4
Nathan-D-George <nathan.intern@bitcube.tech>
2023-09-19 15:41:43 +0200

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,34 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
+
+=begin
+
+  the Joy belonging co
+  So good planetshakers
+
+  Lord of my life
+  Here is my heart
+  You will be saved
+  Been so good
+  Fear is not my future
+  You really are
+  Costly
+  One name
+  More than Able
+  Firm Foundation
+  Jehovah
+  Yahweh
+  Yahweh will Manifest
+  Yeshua
+  Who is this man?
+  Praise (Elevation)
+  Welcome Resurrection (Elevation)
+  Show me your Glory
+  Crowned (highlands worship, Rebecca Hart)
+  All hail the Power of Jesus name
+
+=end
